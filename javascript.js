d8755c18c71ae2413b4af0aa7a08b988a0d96f4a
AA-Stargazer <mforstrtp@protonmail.com>
2022-05-19 21:02:29 +0000

    initial game structure, and place pictures

--- javascript.js
+++ javascript.js
@@ -11,10 +11,12 @@ function bbb() {
 // https://stackoverflow.com/questions/50054982/faster-image-changing-in-js
 
 
+let keepImg = false;
+
+
 let rock_image = document.querySelector("img.rock");
 let paper_image = document.querySelector("img.paper");
 let scissor_image = document.querySelector("img.scissor");
-
 
 let rock_button = document.querySelector(".layer-2 button:nth-child(1)");
 let paper_button = document.querySelector(".layer-2 button:nth-child(2)");
@@ -34,7 +36,8 @@ scissor_button.addEventListener("mouseout", function() { offhover("scissor"); })
 
 
 function onhover(choice) {
-	let element = document.querySelector(`.${choice}`);
+	if (!keepImg) {
+	let element = document.querySelector(`.layer-1.left img.${choice}`);
 	// display is not attribute
 	// https://stackoverflow.com/a/4981472
 	// element.setAttribute("display", "block");
@@ -45,18 +48,108 @@ function onhover(choice) {
 	element.style.opacity = 1;
 
 	// no this and normal one's simple reason connected in my mind... anyway...
+	}
 }
 
 function offhover(choice) {
-	let element = document.querySelector(`img.${choice}`);
+	if (!keepImg) {
+	let element = document.querySelector(`.layer-1.left img.${choice}`);
 	/* element.setAttribute("style", "display: none"); */
 	element.setAttribute("style", "opacity: 0");
+	}
 }
 // ---------------------------------------------
 
 
+let startAndResetButton = document.querySelector(".layer-1.middle button");
+
+// startAndResetButton.addEventListener("click", startGame);  
+
+// TODO create a new boolean value, keep_image. chosen choices' images stays for few seconds on the screen. And while this value is also true, add if statement to startGame, so until images be transparent again, clicked button does nothing (as well as use this in onhover, offhover, so also player images not change...) (of if there is something like time.sleep() in python, we might not need extra 'if' statements... you know, not concurrent etc, but if it is, still nice, we can do lot of other things or just create few extra javascript files and seperate the things that gotta run constantly and that requires some sleep...)...
+// NOTE ok, https://www.sitepoint.com/delay-sleep-pause-wait/ looks like, firstly I need a bit time until come to these (fully understanding, secondly I'll create keep_image boolean (async is good...)
+
+rock_button.addEventListener("click", () => {
+	startGame('rock');	
+});
+paper_button.addEventListener("click", () => {
+	startGame('paper');
+});
+scissor_button.addEventListener("click", () => {
+	startGame('scissor');
+});
+
+let choices = ['rock', 'paper', 'scissor'];
+// choses the random choice and expresses the result
+function startGame(clicked_button) {
+		if (!keepImg){
+		computers_choice = choices[Math.floor(Math.random() * 3)];
+		keepImg = true;
+		showHideImages(clicked_button, computers_choice, 'show');
+		console.log(winnerAlgorithm(clicked_button, computers_choice));
+		setTimeout(
+			() => {
+			keepImg = false;
+			showHideImages(clicked_button, computers_choice, 'hide');
+			}
+			, 3000
+		);
+	}
+}
+
+// maybe I could use switch statement here...
+function winnerAlgorithm(player, computer) {
+	if (
+		(player == "rock" && computer == "paper")
+		||
+		(player == "paper" && computer == "rock")
+	)
+	{
+		if (player == "rock") return 'computer'
+		else return 'player';
+	}
+	else if (
+		(player == "rock" && computer == "scissor")
+		||
+		(player == "scissor" && computer == "rock")
+	)
+	{
+		if (player == "rock") return "player"
+		else return "computer";
+	}
+	else if (
+		(player == "scissor" && computer == "paper")
+		||
+		(player == "paper" && computer == "scissor")
+	)
+	{
+		if (player == "scissor") return "player"
+		else return "computer";
+	}
+	return "tie";
+}
+
+// resets the game
+function resetGame() {
+	
+}
 
 
+function showHideImages(players_image, computers_image, show_or_hide) {
+	if (show_or_hide == 'show') {
+		let playerImg = document.querySelector(`.layer-1.left img.${players_image}`);
+		playerImg.style.opacity = 1;
+		
+		let computerImg = document.querySelector(`.layer-1.right img.${computers_image}`);
+		computerImg.style.opacity = 1;
+	}
+	else if (show_or_hide == 'hide') {
+		let playerImg = document.querySelector(`.layer-1.left img.${players_image}`);
+		playerImg.style.opacity = 0;
+		
+		let computerImg = document.querySelector(`.layer-1.right img.${computers_image}`);
+		computerImg.style.opacity = 0;
+	}
+}
 
 
-
+// TODO make the game min 3 round (until difference is 2), and then reset.... show the result in a box (position: absolute...) (create a element inside to that box)
